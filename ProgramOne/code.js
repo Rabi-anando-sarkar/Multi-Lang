e6a91d7c8459487877f793c87febbfb4b2ecfa93
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-09-30 19:51:26 +0530

    fibonacci series javascript

---
+++ ProgramOne/code.js
@@ -0,0 +1,12 @@
+function fibonacci(num) {
+    const fib = [0,1]
+    for (let i = 2; i < num; i++) {
+        fib[i] = fib[i-1] + fib[i-2]
+    }
+    return fib.toString()
+}
+
+console.log(fibonacci(3));
+console.log(fibonacci(5));
+console.log(fibonacci(7));
+console.log(fibonacci(9));
