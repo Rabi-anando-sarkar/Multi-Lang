583151a88c2e417db4cef4de6a8a85deff457279
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-09-30 21:16:27 +0530

    isPrime javascript

---
+++ ProgramThree/code.js
@@ -0,0 +1,15 @@
+function isPrime(n) {
+    if(n <= 1) {
+        return false
+    }
+    for(let i = 2; i <= Math.sqrt(n);i++){
+        if(n%i === 0) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isPrime(1))
+console.log(isPrime(4))
+console.log(isPrime(5))
