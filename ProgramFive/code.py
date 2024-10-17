0b017892b284a289ce3ffe220ba385e3765d6722
Rabi-anando-sarkar <rabianandos2002@gmail.com>
2024-10-17 10:27:41 +0530

    sort array python

---
+++ ProgramFive/code.py
@@ -0,0 +1,10 @@
+def sortArrayOne(arr):
+    arr.sort()
+    return arr
+
+print(sortArrayOne([7,10,8,11,3,4]))
+
+def sortArrayTwo(arr):
+    return sorted(arr)
+
+print(sortArrayTwo([7,10,8,11,3,4]))
